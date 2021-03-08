5e7ceca15665d06bcf6fb1dc7d2e586eb2231f41
Ian Cossentino <icossentino@gmail.com>
2021-03-08 17:00:16 -0500

    added counties to states display list

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ console.log("test test")
 
 document.addEventListener("DOMContentLoaded", () => {
   getStates()
-  displayStates()
 })
 
+
+
+
+
+
+// Functions to display a state on the homepage
 function getStates() {
   fetch(statesEndpoint)
     .then( resp => resp.json() )
@@ -19,9 +24,15 @@ function displayStates(statesJson) {
 
 function displayState(stateObj) {
   const div1 = document.createElement('div')
-  const ul = document.createElement('ul')
+  const ul1 = document.createElement('ul')
   const li1 = document.createElement('li')
+  const ul2 = document.createElement('ul')
+  const li2 = document.createElement('li')
+  li2.innerText = stateObj.attributes.counties[0].name
   li1.innerText = stateObj.attributes.name
-  ul.appendChild('li1')
-  div1.appendChild('ul')
+  ul2.appendChild(li2)
+  li1.appendChild(ul2)
+  ul1.appendChild(li1)
+  div1.appendChild(ul1)
+  document.body.appendChild(div1)
 }
