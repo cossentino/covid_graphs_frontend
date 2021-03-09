8d344cade5f5acd6d9dbd50eb61c24325af6b06b
Ian Cossentino <icossentino@gmail.com>
2021-03-09 15:12:20 -0500

    changed index display to 'cards' style

--- index.js
+++ index.js
@@ -24,10 +24,24 @@ function displayStates(statesJson) {
 
 function displayState(stateObj) {
   const div1 = document.createElement('div')
-  const ul1 = document.createElement('ul')
-  const li1 = document.createElement('li')
-  li1.innerText = stateObj.attributes.name
-  ul1.appendChild(li1)
-  div1.appendChild(ul1)
+  div1.innerHTML = displayStateCard(stateObj)
   document.querySelector("#app-container").appendChild(div1)
+}
+
+
+function displayStateCard(stateObj) {
+  const card =
+  ` <div class="col s12 m6">
+    <div class="card blue-grey darken-1">
+      <div class="card-content white-text">
+        <span class="card-title">${stateObj.attributes.name}</span>
+        <p>I am a very simple card. I am good at containing small bits of information.
+        I am convenient because I require little markup to use effectively.</p>
+      </div>
+      <div class="card-action">
+        <p>County Breakdown!</p>
+      </div>
+    </div>
+  </div>`;
+return card
 }
