33897780253d6d804f1134a40fbf746de45a3915
Ian Cossentino <icossentino@gmail.com>
2021-03-09 20:28:11 -0500

    creating function to retrieve county-level data for a state

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const statesEndpoint = "http://localhost:3000/api/v1/states"
+const countiesEndpoint = "http://localhost:3000/api/v1/counties"
 console.log("test test")
 
 
@@ -37,13 +38,31 @@ function displayStateCard(stateObj) {
       <div class="card blue-grey darken-1">
         <div class="card-content white-text">
           <span class="card-title">${stateObj.attributes.name}</span>
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
+          <p>Total Cases: ${stateObj.attributes.total_cases}</p>
+          <p>Population: ${stateObj.attributes.population}</p>
         </div>
         <div class="card-action">
-          <p>County Breakdown!</p>
+          <p class="show-state-detail">County Breakdown!</p>
         </div>
       </div>
     </div>`;
 return card
 }
+
+
+// Display detail view of county-by-county breakdown
+
+function getCounties() {
+  fetch(statesEndpoint)
+    .then( resp => resp.json() )
+    .then( json => displayStates(json) )
+}
+
+ const detailLinks = document.querySelectorAll('.show-state-detail')
+
+for (i of detailLinks ) {
+  i.addEventListener('click', () => showCountyView)
+}
+
+function showCountyView() {
+  const stateName = e.target.parentElement.previousElementSibling.querySelector('.card-title').innerHTML
