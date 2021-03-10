63145732bc702e123a0df39daa31819f3899eeb0
Ian Cossentino <icossentino@gmail.com>
2021-03-10 15:16:48 -0500

    allowed toggle between state view and county view

--- index.js
+++ index.js
@@ -13,12 +13,11 @@ function getStates() {
     .then( json => displayStates(json) )
     .then( () => addLinkToCountyButtons() )
 }
-
 function displayStates(statesJson) {
+  document.querySelector('#app-container').innerHTML = ""
   statesJson.data.forEach(s => displayState(s))
   return statesJson
 }
-
 function displayState(stateObj) {
   div1 = document.createElement('div')
   // div1.className = "container col s3"
@@ -26,8 +25,6 @@ function displayState(stateObj) {
   div1.innerHTML = displayStateCard(stateObj)
   document.querySelector("#app-container").appendChild(div1)
 }
-
-
 function displayStateCard(stateObj) {
   const card =
     `<div class="col s3 m3" style="width:300px">
@@ -45,10 +42,7 @@ function displayStateCard(stateObj) {
 return card
 }
 
-
 // Display detail view of county-by-county breakdown
-
-
 
 function addLinkToCountyButtons() {
   const detailLinks = document.querySelectorAll('.show-state-detail')
@@ -58,19 +52,17 @@ function addLinkToCountyButtons() {
     })
   }
 }
-
 function getCountiesByState(e) {
   fetch(`http://localhost:3000/api/v1/states/${e.target.value}/counties`)
     .then( resp => resp.json() )
     .then( json => displayCountyView(json) )
 }
-
 function displayCountyView(json) {
+  document.querySelector('#app-container').innerHTML = ""
   json.data.forEach( countyObj => {
     createCountyCard(countyObj)
   })
 }
-
 function createCountyCard(countyObj) {
   div1 = document.createElement('div')
   // div1.className = "container col s3"
@@ -78,8 +70,6 @@ function createCountyCard(countyObj) {
   div1.innerHTML = createCountyHTML(countyObj)
   document.querySelector("#app-container").appendChild(div1)
 }
-
-
 function createCountyHTML(countyObj) {
   const card =
     `<div class="col s3 m3" style="width:300px">
@@ -96,3 +86,10 @@ function createCountyHTML(countyObj) {
     </div>`;
 return card
 }
+
+
+// Show/hide state view / county view
+
+  function toggleDisplay(elem) {
+    elem.style.visibility = "visible" ? elem.style.visibility = "hidden" : elem.style.visibility = "visible"
+  }
