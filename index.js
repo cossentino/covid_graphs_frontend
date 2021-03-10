65657c410d6723655491713567dee7f398fe0ba9
Ian Cossentino <icossentino@gmail.com>
2021-03-09 21:31:36 -0500

    completed county view - not yet removed states list from county view

--- index.js
+++ index.js
@@ -48,11 +48,6 @@ return card
 
 // Display detail view of county-by-county breakdown
 
-function getCountiesByState(e) {
-  fetch(`http://localhost:3000/api/v1/states/${e.target.value}/counties`)
-    .then( resp => resp.json() )
-    .then( json => showCountyView(e, json) )
-}
 
 
 function addLinkToCountyButtons() {
@@ -64,25 +59,38 @@ function addLinkToCountyButtons() {
   }
 }
 
-function showCountyView(e, json) {
-  const stateName = e.target.parentElement.previousElementSibling.querySelector('.card-title').innerHTML
-  const countiesArray = []
+function getCountiesByState(e) {
+  fetch(`http://localhost:3000/api/v1/states/${e.target.value}/counties`)
+    .then( resp => resp.json() )
+    .then( json => displayCountyView(json) )
+}
+
+function displayCountyView(json) {
   json.data.forEach( countyObj => {
-    displayCountyCard(countyObj)
+    createCountyCard(countyObj)
   })
 }
 
-function displayCountyCard(countyObj) {
+function createCountyCard(countyObj) {
+  div1 = document.createElement('div')
+  // div1.className = "container col s3"
+  div1.style = "width:400px; float:left"
+  div1.innerHTML = createCountyHTML(countyObj)
+  document.querySelector("#app-container").appendChild(div1)
+}
+
+
+function createCountyHTML(countyObj) {
   const card =
     `<div class="col s3 m3" style="width:300px">
       <div class="card blue-grey darken-1">
         <div class="card-content white-text">
-          <span class="card-title">${stateObj.attributes.name}</span>
-          <p>Total Cases: ${stateObj.attributes.total_cases}</p>
-          <p>Population: ${stateObj.attributes.population}</p>
+          <span class="card-title">${countyObj.attributes.name}</span>
+          <p>Total Cases: ${countyObj.attributes.total_cases}</p>
+          <p>Population: ${countyObj.attributes.population}</p>
         </div>
         <div class="card-action">
-          <button class="show-state-detail" value=${stateObj.id}>County Breakdown!</button>
+          <button class="show-county-detail" value=${countyObj.id}>County Breakdown!</button>
         </div>
       </div>
     </div>`;
