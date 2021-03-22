a1efa1bada25d34ca31415c44aaab295aadc9e38
Ian Cossentino <icossentino@gmail.com>
2021-03-22 11:35:54 -0400

    converted form behavior to oojs

--- src/form.js
+++ src/form.js
@@ -1,8 +1,7 @@
 class Form {
-  
- displayNewDataForm() {
-    const myDiv = document.querySelector('#app-container')
-    myDiv.innerHTML = `
+
+ formHTML() {
+    return `
     <form id="add-state-data-form">
       <div class="form-group">
       <label for="state-select"><p class="#">State</p></label>
@@ -23,19 +22,6 @@ class Form {
         <button class="btn btn-primary return-to-home">Home</button>
       </div>
       </form>`
-    document.querySelector('#add-state-data-form').addEventListener('submit', e => submitHandler(e))
-    document.querySelector('.return-to-home').addEventListener('click', () => State.displayStatesView())
-  }
-  
- createStateSelectOptions() {
-    const stateOptions = []
-    const states = State.sortStates()
-    for ( state of states ) {
-      stateOptions.push(
-        `<option value='${state.id}'>${state.name}<option>`
-      )
-    }
-    return stateOptions.join("\n")
   }
   
   
@@ -44,7 +30,7 @@ class Form {
     const state_id = parseInt(document.querySelector('#state-select').value)
     const cases = document.querySelector('#case-input').value
     const date = document.querySelector('#date-input').value
-    postFetchWrapper(state_id, cases, date)
+    this.postFetchWrapper(state_id, cases, date)
   }
   
   
@@ -52,8 +38,8 @@ class Form {
     const myState = State.all.find(el => el.id == state_id )
     const bodyData1 = { state_id: myState.id, cases: cases, date: date }
     const bodyData2 = { id: myState.id, total_cases: parseInt(myState.total_cases) + parseInt(cases) }
-    addDataDay(myState, bodyData1)
-    updateTotalCases(myState, bodyData2)
+    this.addDataDay(myState, bodyData1)
+    this.updateTotalCases(myState, bodyData2)
   }
   
  addDataDay(state, body) {
--- src/index.js
+++ src/index.js
@@ -63,28 +63,9 @@ function getStateGraph(e) {
 
 function displayNewDataForm() {
   const myDiv = document.querySelector('#app-container')
-  myDiv.innerHTML = `
-  <form id="add-state-data-form">
-    <div class="form-group">
-    <label for="state-select"><p class="#">State</p></label>
-      <select id="state-select" style="display:inline-block">
-        ${createStateSelectOptions()}
-      </select>
-    </div>
-    <div class="form-group">
-      <label for="case-input"><p class="#">Cases</p></label>
-      <input type="text" class="form-control" id="case-input">
-    </div>
-    <div class="form-group">
-      <label for="date-input">Date</label>
-      <input type="date" class="form-control" id="date-input">
-    </div>
-    <div class="row">
-      <button type="submit" id="add-data-submit" class="btn btn-primary">Submit</button>
-      <button class="btn btn-primary return-to-home">Home</button>
-    </div>
-    </form>`
-  document.querySelector('#add-state-data-form').addEventListener('submit', e => submitHandler(e))
+  const form = new Form
+  myDiv.innerHTML = form.formHTML()
+  document.querySelector('#add-state-data-form').addEventListener('submit', e => form.submitHandler(e))
   document.querySelector('.return-to-home').addEventListener('click', () => State.displayStatesView())
 }
 
@@ -99,46 +80,4 @@ function createStateSelectOptions() {
   return stateOptions.join("\n")
 }
 
-
-function submitHandler(e) {
-  e.preventDefault()
-  const state_id = parseInt(document.querySelector('#state-select').value)
-  const cases = document.querySelector('#case-input').value
-  const date = document.querySelector('#date-input').value
-  postFetchWrapper(state_id, cases, date)
-}
-
-
-function postFetchWrapper(state_id, cases, date) {
-  const myState = State.all.find(el => el.id == state_id )
-  const bodyData1 = { state_id: myState.id, cases: cases, date: date }
-  const bodyData2 = { id: myState.id, total_cases: parseInt(myState.total_cases) + parseInt(cases) }
-  addDataDay(myState, bodyData1)
-  updateTotalCases(myState, bodyData2)
-}
-
-function addDataDay(state, body) {
-fetch(`http://localhost:3000/api/v1/states/${state.id}/state_days`, {
-  method: "POST",
-  headers: {"Content-Type": "application/json"},
-  body: JSON.stringify(body)
-})
-  .then(resp => resp.json())
-  .then(() => {
-    state.state_days.push(body)
-  })
-}
-
-function updateTotalCases(state, body) {
-  fetch(`http://localhost:3000/api/v1/states/${state.id}`, {
-    method: "PATCH", 
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(body)
-  })
-  .then(resp => resp.json())
-  .then(() => {
-    state.total_cases += parseInt(document.querySelector('#case-input').value)
-    state.displayOneState()
-  })
-}
 
